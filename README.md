e1572b408ea49490f5abc5f0bb8852dfe30331e5
Erick Navarro <erick@navarro.io>
2024-06-10 17:05:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# treesit-ispell.el
+
+Run ispell on text nodes, this works using `treesit` built-in package.
+
+## Installation
+
+### Cloning the repo
+
+Clone this repo somewhere, and add this to your config:
+
+```elisp
+(add-to-list 'load-path "path where the repo was cloned")
+
+(require 'treesit-ispell)
+```
+
+### Using straight.el
+
+```emacs-lisp
+(use-package treesit-ispell
+  :straight (treesit-ispell
+             :type git
+             :host github
+             :repo "erickgnavar/treesit-ispell.el"))
+```
+
+## Usage
+
+`M-x treesit-ispell-run-at-point` to use the node at the current position
+
+It can also be attached to a keybinding, for example:
+
+```emacs-lisp
+(global-set-key (kbd "C-x C-s") 'treesit-ispell-run-at-point)
+```
---
+++ treesit-ispell.el
@@ -0,0 +1,56 @@
+;;; treesit-ispell.el --- Run ispell on tree-sitter text nodes -*- lexical-binding: t -*-
+
+;; Copyright © 2024 Erick Navarro
+;; Author: Erick Navarro <erick@navarro.io>
+;; URL: https://github.com/erickgnavar/treesit-ispell.el
+;; Version: 0.1.0
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; Package-Requires: ((emacs "29.1"))
+
+;;; Commentary:
+;; Run spell check against treesit text nodes
+
+;;; Code:
+
+(require 'seq)
+(require 'cl-lib)
+(require 'treesit)
+
+;;TODO: Add more languages, inspect their grammars and define their text elements
+(defcustom treesit-ispell-grammar-text-mapping '((python-ts-mode . (string_content comment))
+                                                 (dockerfile-ts-mode . (comment))
+                                                 (elixir-ts-mode . (quoted_content comment)))
+  "All the supported text elements for each grammar."
+  :type '(alist :key-type symbol :value-type sexp)
+  :group 'treesit-ispell)
+
+(defun treesit-ispell--check-loaded-grammar ()
+  "Check if treesit is available and if there is a grammar loaded already."
+  (unless (treesit-language-at (point))
+    (user-error "There is no grammar loaded for this mode")))
+
+(defun treesit-ispell--get-text-node-at-point ()
+  "Get text node at point using predefined major mode options."
+  (let ((types (alist-get major-mode treesit-ispell-grammar-text-mapping)))
+    (seq-some (lambda (x)
+                (let* ((lang (treesit-language-at (point)))
+                       (node (treesit-node-at (point) lang))
+                       (query (treesit-query-compile lang (format  "((%s) @%s)" x x)))
+                       (capture (treesit-query-capture node query)))
+                  (and capture node))) types)))
+
+(defun treesit-ispell--run-ispell-on-node (node)
+  "Run ispell over the text of the received `NODE'."
+  (ispell-region (treesit-node-start node) (treesit-node-end node)))
+
+;;;###autoload
+(defun treesit-ispell-run-at-point ()
+  "Run ispell at current point if there is a text node."
+  (interactive)
+  (treesit-ispell--check-loaded-grammar)
+  (when-let ((node (treesit-ispell--get-text-node-at-point)))
+    (treesit-ispell--run-ispell-on-node node)))
+
+(provide 'treesit-ispell)
+
+;;; treesit-ispell.el ends here
